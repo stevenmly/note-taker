2e0d2d2ae16cdb5e9138cabd35d98ba2d5fcfb0c
Steven Ly <ly.steven16@gmail.com>
2021-07-19 02:50:12 -0500

    Added middleware, imported packages, and functions to parse incoming data in server.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 
 const express = require('express');
 const app = express();
 
 let notes = require("./db/db.json");
+
+//middleware
+app.use(express.static('./public'));
+
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+
+// parse incoming JSON data
+app.use(express.json());
+
 
 app.get('/notes', (req, res) => {
     res.json(notes);
